a14673bed8eb9e18f8a740aa510886b2ef7bc3ef
slamets24 <59117539+slamets24@users.noreply.github.com>
2019-12-21 21:56:25 +0700

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,18 @@
+<?php
+function bilang(){
+    for($i=1;$i<=100;$i++){
+        $a=0;
+        for($j=1;$j<=$i;$j++){
+            if($i % $j == 0){
+                $a++;
+            }
+        }
+
+        if($a == 2){
+            echo $i.", ";
+        }
+    }
+}
+
+bilang(3,4)
+?>
